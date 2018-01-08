8d45ddc8f5f739bb74747467a55a86251405f8ce
Sebastian9112 <35230658+Sebastian9112@users.noreply.github.com>
2018-01-08 18:34:38 +0100

    Add files via upload

---
+++ module_constants.py
@@ -0,0 +1,29 @@
+#MBAC begin
+hack_unblockable = 0
+hack_autoblock = 1
+hack_aimbot = 2
+num_hacks = 3
+ 
+slot_troop_hack_percentage = 1000
+slot_troop_item_attack_dir_caps = 1001
+ 
+slot_player_unblockable_warnings = 1000
+slot_player_autoblock_warnings = 1001
+slot_player_aimbot_warnings = 1002
+ 
+slot_item_num_attacks = 1000
+ 
+slot_agent_tick_check_time = 1000
+slot_agent_defend_dir = 1001
+slot_agent_defend_ticks = 1002
+ 
+slot_agent_period_reset_time = 1003
+ 
+slot_agent_period_melee_hits = 1004
+slot_agent_period_blocks = 1005
+slot_agent_period_ranged_hits = 1006
+ 
+slot_agent_period_unblocks = 1007
+slot_agent_period_down_blocks = 1008
+slot_agent_period_head_shots = 1009
+#MBAC end
---
+++ module_strings.py
@@ -0,0 +1,16 @@
+#MBAC begin
+strings += [
+  ("hack_0","Unblockable"),
+  ("hack_1","Autoblock"),
+  ("hack_2","Aimbot"),
+
+  ("hack_0_log","[melee hits({reg0}) unblocks({reg1}) {reg2}%]"),
+  ("hack_1_log","[blocks({reg0}) down blocks({reg1}) {reg2}%]"),
+  ("hack_2_log","[ranged hits({reg0}) head shots({reg1}) {reg2}%]"),
+
+  ("hack_consequence-1","detected"),
+  ("hack_consequence_0","permanently banned"),
+  ("hack_consequence_1","temporarily banned"),
+  ("hack_consequence_2","kicked"),
+]
+#MBAC end
