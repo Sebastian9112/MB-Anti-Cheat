64390bdd71bba13b2fece66bf51fda476ec50a66
Sebastian9112 <35230658+Sebastian9112@users.noreply.github.com>
2018-01-08 18:45:09 +0100

    Update to v0.8.1

--- module_constants.py
+++ module_constants.py
@@ -1,28 +1,44 @@
 #MBAC begin
+mbac_v_maj = 0
+mbac_v_min = 8
+mbac_v_rev = 1
+
+msg_log_only = -1
+msg_players = 0
+msg_admins = 1
+
 hack_unblockable = 0
 hack_autoblock = 1
 hack_aimbot = 2
 num_hacks = 3
- 
-slot_troop_hack_percentage = 1000
-slot_troop_item_attack_dir_caps = 1001
- 
+
+cmd_hack_options = 0
+cmd_advanced_logging = num_hacks
+cmd_player_join_message = num_hacks + 1
+cmd_hack_consequence = num_hacks + 2
+cmd_broadcast_hack_warnings = num_hacks + 3
+cmd_broadcast_hack_consequences = num_hacks + 4
+
+slot_troop_enable_anti_cheat = 1000
+slot_troop_hack_percentage = 1001
+slot_troop_item_attack_dir_caps = 1002
+
 slot_player_unblockable_warnings = 1000
 slot_player_autoblock_warnings = 1001
 slot_player_aimbot_warnings = 1002
- 
+
 slot_item_num_attacks = 1000
- 
+
 slot_agent_tick_check_time = 1000
 slot_agent_defend_dir = 1001
 slot_agent_defend_ticks = 1002
- 
+
 slot_agent_period_reset_time = 1003
- 
+
 slot_agent_period_melee_hits = 1004
 slot_agent_period_blocks = 1005
 slot_agent_period_ranged_hits = 1006
- 
+
 slot_agent_period_unblocks = 1007
 slot_agent_period_down_blocks = 1008
 slot_agent_period_head_shots = 1009
